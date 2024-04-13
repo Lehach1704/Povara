1bc9e32ab73efb3e468ee8a3d79e354dac892a77
Alexey Kolotkov <alex.colotkov@yandex.ru>
2024-04-13 12:48:46 +0500

    Составлен один из 2-х контейнеров

--- main.cpp
+++ main.cpp
@@ -1,11 +1,86 @@
 #include <iostream>
-#include "povara.h"
+#include <vector>
 
 using namespace std;
 
+#include "povara.h"
 
+CookerContainer::CookerContainer(int maxSize)
+{
+    CookerBox = new CookerPtr[maxSize];
+    for(int i = 0; i < MaxSize; i++)
+    {
+        CookerBox[i] = NULL;
+    }
+    CookerCount = 0;
+    MaxSize = maxSize;
+}
+
+wstring PrintCookerType(const Cooker type)
+{
+    switch(type)
+    {
+        case Cooker::Mouse: return L"Мышь";
+        case Cooker::Cat: return L"Кот";
+        case Cooker::Robot: return L"Робот";
+        case Cooker::WheelchairUser: return L"Колясочник";
+        default: return L"неизвестно";
+    }
+}
+
+void CookerContainer::AddCooker(CookerPtr newCooker)
+{
+    CookerBox[CookerCount++] = newCooker;
+}
+
+void Task1(CookerContainer *cookerBox)
+{
+    for(int i=0; i<cookerBox->GetCount(); i++)
+    {
+        const CookerPtr outCook  = cookerBox->GetByIndex(i); // outCook = currentFruit
+        wcout << i << L" (" << PrintCookerType(outCook->GetCooker()) << L")" << endl;
+    }
+
+
+}
 
 int main()
 {
+    setlocale(LC_ALL, "Russian");
+    CookerContainer cookerBox(100);
+    for(int i=0; i<50; i++)
+    {
+        cookerBox.AddCooker(new Mouse());
+    }
 
+    for(int i=0; i<10; i++)
+    {
+        cookerBox.AddCooker(new Cat());
+    }
+
+    for(int i=0; i<10; i++)
+    {
+        cookerBox.AddCooker(new Robot());
+    }
+
+    for(int i=0; i<30; i++)
+    {
+        cookerBox.AddCooker(new WheelchairUser());
+    }
+
+    Task1(&cookerBox);
+    /*for(int i=0; i<10; i++)
+    {
+        fruitBox.AddFruit(new Apple(ColorEnum::Red));
+    }
+    for(int i=0; i<20; i++)
+    {
+        fruitBox.AddFruit(new Kiwi);
+    }
+    for(int i=0; i<20; i++)
+    {
+        fruitBox.AddFruit(new Orange);
+    }*/
+
+    return 0;
 }
--- povara.h
+++ povara.h
@@ -1,10 +1,19 @@
 #ifndef PovaraH
 #define PovaraH
 #include <string>
+#include <vector>
 
 using namespace std;
 
-class ActionOfCooker
+enum class Cooker: int
+{
+    Mouse,
+    Cat,
+    Robot,
+    WheelchairUser
+};
+
+/*class ActionOfCooker
 {
 protected:
      int Skill;
@@ -16,9 +25,28 @@ public:
      virtual void CookOff() const = 0; // не готовка
      virtual void CookON() const = 0; // готовка
      virtual void Dis() const = 0; // увольнение
-     virtual void VacON() const = 0;
-     virtual void VacOff() const = 0; // отправка в отпуск
+     virtual void VacON() const = 0; // отправка в отпуск
+     virtual void VacOff() const = 0; // вернуть из отпуска
+};*/
+
+class ActionOfCooker
+{
+protected:
+     int Skill;
+     int Stazh;
+     int Age;
+     string Sex;
+     string Post;
+public:
+     void CookOff(); // не готовка
+     void CookON(); // готовка
+     void Dis(); // увольнение
+     void VacON(); // отправка в отпуск
+     void VacOff(); // вернуть из отпуска
+     virtual Cooker GetCooker() const = 0; // определение повара
 };
+
+typedef ActionOfCooker * CookerPtr;
 
 class Mouse: public ActionOfCooker
 {
@@ -38,8 +66,7 @@ public:
     void Dism(); // таракан
     void VacON(); // отправка в отпуск на отсыпание
     void VacOff(); // возврат из отпуска
-
-
+    Cooker GetCooker() const {return Cooker::Mouse;}
 };
 
 class Cat: public ActionOfCooker
@@ -55,11 +82,12 @@ private:
 public:
     Cat();
     virtual ~Cat() {};
-    void CookON(); // не готовит суши
+    void CookON(); // готовит суши
     void CookOff(); // не готовит суши
     void Dism(); // курение
     void VacON(); // отправка в отпуск на Канары
     void VacOff(); // возврат из отпуска
+    Cooker GetCooker() const {return Cooker::Cat;}
 };
 
 class Robot: public ActionOfCooker
@@ -80,6 +108,7 @@ public:
     void Dism(); // взломал базу Пентагона
     void VacON(); // отправка на отпуск к семье
     void VacOff(); // возврат из отпуска
+    Cooker GetCooker() const {return Cooker::Robot;}
 };
 
 class WheelchairUser: public ActionOfCooker
@@ -100,6 +129,43 @@ public:
     void Dism(); // спалил кухню
     void VacON(); // отправка на Кубу
     void VacOff(); // возврат из отпуска
+    Cooker GetCooker() const {return Cooker::WheelchairUser;}
+};
+
+/*class CookerContIterator1 : public Iterator<FruitPtr>
+{
+private:
+    const  FruitPtr *FruitBox;
+    int Pos;
+    int Count;
+
+public:
+    FruitContainerIterator(const  FruitPtr *fruitBox, int count)
+    {
+        FruitBox = fruitBox;
+        Count = count;
+        Pos = 0;
+    }
+
+    void First() {Pos = 0;}
+    void Next() {Pos++;}
+    bool IsDone() const {return Pos >= Count;}
+    FruitPtr GetCurrent() const {return FruitBox[Pos];}
+};*/
+
+class CookerContainer
+{
+private:
+    CookerPtr *CookerBox;
+    int CookerCount;
+    int MaxSize;
+public:
+    CookerContainer(int maxSize);
+    /*virtual ~CookerContainer();*/
+    void AddCooker (CookerPtr newCooker);
+    int GetCount() const { return CookerCount; }
+    CookerPtr GetByIndex(int index) const { return CookerBox[index]; }
+
 };
 
 #endif // PovaraH
